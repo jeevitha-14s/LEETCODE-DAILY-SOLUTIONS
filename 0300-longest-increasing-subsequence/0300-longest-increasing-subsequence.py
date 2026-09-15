319b6a49a71ee339111a2604355e411698151923
Jeevitha S <sjeevitha679@gmail.com>
2026-09-15 21:22:49 +0530

    Time: 0 ms (100%), Space: 19.6 MB (33.12%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,10 @@
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        tails = []
+        for x in nums:
+            i = bisect_left(tails, x)
+            if i == len(tails):
+                tails.append(x)
+            tails[i] = x
+        return len(tails)
