06efa6441fafed62f499913875bd4e4f654513f4
Jeevitha S <sjeevitha679@gmail.com>
2026-09-16 10:10:29 +0530

    Time: 1497 ms (84.98%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,1 @@
+SELECT 'Low Salary' AS category, IFNULL(SUM(income < 20000), 0) AS accounts_count FROM Accounts UNION ALL SELECT 'Average Salary', IFNULL(SUM(income BETWEEN 20000 AND 50000), 0) FROM Accounts UNION ALL SELECT 'High Salary', IFNULL(SUM(income > 50000), 0) FROM Accounts;
